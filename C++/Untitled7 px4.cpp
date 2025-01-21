632963f65c9610396f41b4587967dff8d18c1577
Elena-Om <elin.elena@gmail.com>
2025-01-21 20:58:32 +0200

    Add files via upload

---
+++ C++/Untitled7 px4.cpp
@@ -0,0 +1,52 @@
+// Pradeigma 4
+
+#include <iostream>
+using namespace std;
+
+class Circle {
+	public:
+		double aktina;
+		Circle();
+		~Circle();
+		
+		void set_aktina (double x)
+		{
+			aktina=x;
+		}
+		
+		double give_embado ()
+		{
+			return 3.14*aktina*aktina;
+		}
+};
+
+Circle::Circle()
+{
+	aktina=1;
+}
+
+Circle::~Circle()
+{
+}
+
+int main()
+{
+	Circle d[4];
+	double a;
+	int i;
+	
+	for(i=0;i<4;i++)
+	{
+		cout << "Dwse thn timi tis aktinas: " << endl;
+		cin >> a;
+		d[i].set_aktina(a);
+	}
+	
+	for(i=0;i<4;i++)
+	{
+		cout << "To emvadon einai: " << d[i].give_embado() << endl;
+	}
+	
+	return 0;
+}
+
