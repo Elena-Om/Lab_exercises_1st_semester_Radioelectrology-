35a03617bc67713b02c584371fae6a181f84a704
Elena-Om <elin.elena@gmail.com>
2025-01-21 21:20:08 +0200

    Add files via upload

---
+++ C++/Untitled11.cpp
@@ -0,0 +1,43 @@
+/* Kanw dio migadikous sthn main (double) kai ftiaxnw kai 3o migadiko */
+
+#include <iostream>
+using namespace std;
+
+class Migas {
+	public: 
+	   double x;
+	   double y;
+	   
+	   Migas(double a=1,double b=1);
+	   ~Migas();
+	   
+	   Migas operator+(Migas M2);
+};
+
+Migas::Migas(double a,double b)
+{
+	x=a;
+	y=b;
+}
+Migas::~Migas(){}
+
+Migas Migas::operator+(Migas M2)
+{
+	Migas migadikos;
+	migadikos.x= this->x + M2.x; //Des kai enallaktiko tropo me deikti
+	migadikos.y= this->y + M2.y;
+	return migadikos;
+}
+
+int main()
+{
+	Migas M1(1,2);
+	Migas M2(3,4);
+	Migas M3;
+	
+	M3 = M1+M2;
+	
+	cout << "To athroisma twn migadikwn einai: " << M3.x << "+i" << M3.y << endl;
+	
+	return 0;
+}
