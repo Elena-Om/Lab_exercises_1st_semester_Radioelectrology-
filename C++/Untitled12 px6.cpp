83ed5362b6135279ae48cf1363f662b3c8670349
Elena-Om <elin.elena@gmail.com>
2025-01-21 21:00:12 +0200

    Add files via upload

---
+++ C++/Untitled12 px6.cpp
@@ -0,0 +1,71 @@
+// Paradeigma 6
+// Diskoli
+
+#include <iostream>
+using namespace std;
+
+class Multifaceted {
+	public:
+		int pleures;
+		double *mikos;
+		
+		Multifaceted();
+		~Multifaceted();
+		
+		void set_pleura (int x)
+		{
+			pleures=x;
+			mikos=new double[pleures];
+		}
+		
+		void set_mikos(int a,double b)
+		{
+			*(mikos+a)=b;
+		}
+		
+		double perimetros()
+		{
+			int i=0;
+			double perimetros=0;
+			
+			for(i=0;i<pleures;i++)
+			{
+				perimetros=perimetros+*(mikos+i);
+			}
+			return perimetros;
+		}
+};
+
+Multifaceted::Multifaceted()
+{
+	//cout << "DIMIOURGIA POLIPLEUROU" << endl; 
+	pleures=0;
+}
+
+Multifaceted::~Multifaceted()
+{
+	//cout << "KATASTROFI POLIPLEUROU" << endl;
+	delete [] mikos;
+}
+
+int main()
+{
+	Multifaceted sxima;
+	int plithos,i=0;
+	double mhkos;
+	
+	cout << "Dwse arithmo pleurwn: " << endl;
+	cin >> plithos;
+	sxima.set_pleura(plithos);
+	
+	for(i=0;i<plithos;i++)
+	{
+		cout << "Dwse to mikos ths " << i+1 << "s pleuras: " << endl;
+		cin >> mhkos;
+		sxima.set_mikos(i,mhkos);
+	}
+	
+	cout << "H perimetros tou polipleurou einai: " << sxima.perimetros() << endl;
+	
+	return 0;
+}
